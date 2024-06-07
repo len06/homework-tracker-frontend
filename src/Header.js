472180de2efbeb4fa3f0f4af41b6454ea7010da5
joserigel <joserigel18@gmail.com>
2024-06-07 23:00:18 +0200

    Added button to Home

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import logo from './images/logo.jpg'
 import './Header.css'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Header(){
     return(
         <header>
             <nav className="nav_bar">
-                <img className="nav_logo"src={logo} alt="Homework Tracker Logo"/>
-                <h3 className='nav_title'>Homework Tracker</h3>
+                <NavLink exact to='/' className="nav_logo">
+                    <img src={logo} alt="Homework Tracker Logo"/>
+                    <h3>Homework Tracker</h3>
+                </NavLink>
                 <ul className='nav_options'>
                     <li><Link>Support</Link></li>
                     <li><Link to='/login'>Log In</Link></li>
